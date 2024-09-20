7c1def87f8b90421ca615bf6a30fedbd3958419a
lnwchok <sitthichok.punlabpho@gmail.com>
2024-09-20 17:34:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from openpyxl import Workbook, load_workbook
 filename = r"C:\Users\QX6-XXF1808D002\Desktop\Doc List.xlsx"
 
 wb = load_workbook(filename, read_only=True, keep_vba=False)
-ws = wb.active
+ws = wb["MESA Doc List to EVLS"]
+
+data = ws["B6"].value
+
+print(data)
